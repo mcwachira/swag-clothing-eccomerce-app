f949703a7a5e3e6fb9b91e7c67b24c583af34cf7
mcwachira <23430963+mcwachira@users.noreply.github.com>
2022-03-17 16:48:38 +0300

    added a header component to our app and links to our shop  and contact pages

--- src/App.js
+++ src/App.js
@@ -4,20 +4,15 @@ import './App.css';
 import HomePage from './pages/homepage/HomePage.component'
 import { Route, Routes } from 'react-router-dom';
 import Shop from './pages/shop/shop.component';
+import Header from './components/header/header.component';
 
-const HatsPage = () => {
-  return (
-    <div> <h1>
-      Hats Page
-    </h1>
-    </div>
-  )
-}
+
 function App() {
   return (
     <div>
 
 
+      <Header />
 
       <Routes>
         <Route path="/" element={<HomePage />} />
