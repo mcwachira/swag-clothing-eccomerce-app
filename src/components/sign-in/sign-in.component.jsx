f2234161550459396a6337afc2a0c914d0a8dea8
mcwachira <23430963+mcwachira@users.noreply.github.com>
2022-03-19 20:07:59 +0300

    added sign in with email method to my app

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -3,7 +3,8 @@ import React, { Component } from 'react';
 import './sign-in.styles.scss'
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
-import { signInWithGoogle } from '../../firebase/firebase.utils';
+import { auth, signInWithGoogle } from '../../firebase/firebase.utils';
+
 class SignIn extends Component {
 
     constructor(props) {
@@ -14,12 +15,21 @@ class SignIn extends Component {
         }
     }
 
-    handleSubmit = (e) => {
+    handleSubmit = async (e) => {
         e.preventDefault()
-        this.setState({
-            email: "",
-            password: ""
-        })
+
+        const { email, password } = this.state;
+        try {
+            await auth.signInWithEmailAndPassword(email, password)
+            this.setState({
+                email: "",
+                password: "",
+            })
+        } catch (error) {
+            console.log(error)
+        }
+
+
     }
 
     //This function takes the value from our input (email and password ) and updates the state by  assigning the name(email or password ) to its value
