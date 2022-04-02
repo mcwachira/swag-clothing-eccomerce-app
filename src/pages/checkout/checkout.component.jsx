2d9efc1d3a0ed57dbf6189828897d6541bd1794d
mcwachira <23430963+mcwachira@users.noreply.github.com>
2022-04-02 10:35:05 +0300

    made changes to the checkout component

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 import { connect } from 'react-redux'
 import { createStructuredSelector } from 'reselect'
 import { selectCartItems, selectCartTotal } from '../../redux/cart/cart.selectors'
-import CheckoutItem from '../../components/checkout-item/checkout-item,component'
+import CheckoutItem from '../../components/checkout-item/checkout-item.component'
 import StripeCheckoutButton from '../../components/stripe/stripe-button.component'
 
 import './checkout.styles.scss'
