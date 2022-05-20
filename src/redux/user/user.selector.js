6b7a06bd30bad2ed7e4812d1800266cb68826eac
mcwachira <23430963+mcwachira@users.noreply.github.com>
2022-05-20 19:43:40 +0300

    started a fresh building the Ecommerce app using react,redyx,graphql and firebase

--- src/redux/user/user.selector.js
+++
@@ -1,9 +0,0 @@
-import { createSelector } from "reselect";
-
-const SelectUser = state => state.user;
-
-export const selectCurrentUser = createSelector(
-
-    [SelectUser],
-    (user) => user.currentUser
-)
