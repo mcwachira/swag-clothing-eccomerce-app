392adaf6a39773920bf7671a41cbc303fcffcd64
mcwachira <23430963+mcwachira@users.noreply.github.com>
2022-05-20 23:59:17 +0300

    added react-router and navigation component to our app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
+import Home from "./routes/home/home .component";
+import { Routes, Route } from 'react-router-dom'
+import Navigation from "./routes/navigation/navigation.component";
 
-import Directory from "./components/directory/directory.component";
+
 const App = () => {
 
   return (
 
     <div>
-      <Directory />
+
+      <Routes>
+        <Route path="/" element={<Navigation />}>
+          <Route index={true} element={<Home />} />
+        </Route>
+
+      </Routes>
+
     </div>
 
   );
---
+++ src/routes/navigation/navigation.component.jsx
@@ -0,0 +1,30 @@
+
+import React, { Fragment } from "react"
+import { Link, Outlet } from 'react-router-dom'
+import { ReactComponent as SwagLogo } from '../../assets/crown.svg'
+import './navigation.styles.scss'
+import { ReactComponent as ShoppingBag } from '../../assets/shopping-bag.svg'
+const Navigation = () => {
+    return (
+        <Fragment>
+            <div className="navigation">
+                <Link className="logo-container" to="/">
+                    <div>
+                        <SwagLogo className="logo" />
+                    </div>
+                </Link>
+                <div className="nav-links-container">
+                    <Link className="nav-link" to='/shop'>Shop</Link>
+                </div>
+            </div>
+            <Outlet />
+            {/* outlet is used to persist the navigation bar */}
+        </Fragment>
+
+
+
+
+    )
+}
+
+export default Navigation
