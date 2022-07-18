6fc0c960a6d1f269f06a31c1931958ce926b5a7b
mcwachira <23430963+mcwachira@users.noreply.github.com>
2022-07-18 12:38:29 +0300

    updated the userContext to use the use Reducer hook In place of use State

--- src/contexts/user.context.jsx
+++ src/contexts/user.context.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState, useEffect } from 'react'
+import { createContext, useState, useEffect, useReducer } from 'react'
 import { onAuthStateChangedListener, createUserDocumentFromAuth } from '../utils/firebase/firebase.utils'
 
 
@@ -11,9 +11,48 @@ export const UserContext = createContext({
 
 
 
+export const USER_ACTION_TYPES = {
+    SET_CURRENT_USER: 'SET_CURRENT_USER'
+
+}
+
+
+const userReducer = (state, action) => {
+
+    console.log('dispatch')
+    console.log(action)
+    const { type, payload } = action
+
+    switch (type) {
+        case USER_ACTION_TYPES.SET_CURRENT_USER:
+            return {
+                ...state,
+                currentUser: payload
+            }
+
+        default:
+            throw new Error(`unhandled type ${type} in userReducer`);
+    }
+
+}
+
+
+const INITIAL_STATE = {
+    currentUser: null
+}
+
 export const UserProvider = ({ children }) => {
 
-    const [currentUser, setCurrentUser] = useState(null)
+    // const [currentUser, setCurrentUser] = useState(null)
+    const [ {currentUser}, dispatch  ] = useReducer(userReducer, INITIAL_STATE)
+
+    // const { currentUser } = state;
+    const setCurrentUser = (user) =>  dispatch({
+        type: USER_ACTION_TYPES.SET_CURRENT_USER,
+        payload: user
+
+    })
+
     useEffect(() => {
 
         //used to clean up the method
