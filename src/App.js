b1aa07e4d4743f517541bb1f1197f23e87120616
mcwachira <23430963+mcwachira@users.noreply.github.com>
2023-03-09 10:10:39 +0300

    updating the app to a new design

--- src/App.js
+++ src/App.js
@@ -1,51 +1,9 @@
-import { useEffect } from "react";
-import Home from "./routes/home/home .component";
-import { Routes, Route } from 'react-router-dom'
-import Navigation from "./routes/navigation/navigation.component";
-import Authentication from "./routes/auth/authentication.component";
-import Shop from "./routes/shop/shop.component";
-import CheckOut from "./routes/checkout/checkout.component";
-import { onAuthStateChangedListener, createUserDocumentFromAuth } from './utils/firebase/firebase.utils'
-import { setCurrentUser } from "./redux/user/user.action";
-import { useDispatch } from "react-redux";
+import React from 'react'
+
 const App = () => {
-const dispatch = useDispatch()
-
-  useEffect(() => {
-
-    //used to clean up the method
-    const unsubscribe = onAuthStateChangedListener((user) => {
-      if (user) {
-        createUserDocumentFromAuth(user)
-      }
-      console.log(user)
-      dispatch(setCurrentUser(user))
-    })
-    return unsubscribe
-  }, [dispatch])
-
-
   return (
-
-    <div>
-
-      <Routes>
-        <Route path="/" element={<Navigation />}>
-          <Route index={true} element={<Home />} />
-          <Route path="shop/*" element={<Shop />} />
-          <Route path="/authentication" element={<Authentication />} />
-
-          <Route path="/checkout" element={<CheckOut />} />
-
-        </Route>
-
-
-
-      </Routes>
-
-    </div>
-
-  );
+    <div>App</div>
+  )
 }
 
-export default App;
+export default App
--- src/redux/root-saga.js
+++
@@ -1,8 +0,0 @@
- import {all, call } from 'redux-saga/effects'
- import { categorySaga } from './category/category.saga'
-
- //generator function 
- export  function* rootSaga(){
-
-    yield all[call(categorySaga)]
- }
