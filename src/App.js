bd2c2b6620f99ef7ac70a9f377394c0382a77840
mcwachira <23430963+mcwachira@users.noreply.github.com>
2023-03-09 12:01:32 +0300

    created the diectory page
    Added the category item component and directory component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,48 @@
 import React from 'react'
+import Directory from './components/directory/directory.component'
 
 const App = () => {
+
+
+  const categories = [
+    {
+      "id": 1,
+      "title": "Hats",
+      "imageUrl": "./images/hats.png",
+    },
+    {
+      "id": 2,
+      "title": "Jackets",
+      "imageUrl": "./images/jackets.png",
+      "price": 18
+    },
+    {
+      "id": 3,
+      "title": "Sneakers",
+      "imageUrl": "./images/sneakers.png",
+
+    },
+    {
+      "id": 4,
+      "title": "Womens",
+      "imageUrl": "./images/womens.png",
+    },
+    {
+      "id": 5,
+      "title": "Mens",
+      "imageUrl": "./images/mens.png",
+      "price": 18
+    },
+
+  ]
+
   return (
-    <div>App</div>
+
+ <>
+ <Directory categories={categories}/>
+ </>
+
+  
   )
 }
 
